69581b532cd9f3b8e7f7588701c8e93f2780ff4f
byurudur <batuhanyurudur@gmail.com>
2024-02-27 01:39:38 +0300

    Bug-Fix process

--- week2/boxing-game/src/Main.java
+++ week2/boxing-game/src/Main.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
 
-        Game fighter = new Game("Tayyip", 100, "Kemal", 100);
+        Game fighter = new Game("Mike Tyson", 100, "Roberto Duran", 100);
 
         if (fighter.weight1 != fighter.weight2){
             System.out.println("The weights of the fighters must be equal.");
