bf2248f22677fe9df1629513960cdafcaf6eabe7
byurudur <batuhanyurudur@gmail.com>
2024-02-27 00:49:09 +0300

    The divisor has been changed to another value

--- week2/recursive-prime-numbers/src/Main.java
+++ week2/recursive-prime-numbers/src/Main.java
@@ -1,30 +1,27 @@
 import java.util.Scanner;
 
 public class Main {
-    public static boolean isPrime(int num, int divisor) {
+    public static boolean isPrime(int num, int divider) {
         if (num <= 2) {
             return (num == 2) ? true : false;
-        }
-        else if (num % divisor == 0) {
+        } else if (num % divider == 0) {
             return false;
-        }
-        else if (divisor * divisor > num) {
+        } else if (divider * divider > num) {
             return true;
         }
-        return isPrime(num, divisor + 1);
+        return isPrime(num, divider + 1);
     }
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Please enter a number: ");
-            int number = scanner.nextInt();
 
-            if (isPrime(number, 2)){
-                System.out.println(number + " is a prime number.");}
-            else {
-                System.out.println(number + " is NOT a prime number.");
-                scanner.close();
-            }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter a number: ");
+        int number = scanner.nextInt();
+
+        if (isPrime(number, 2)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is NOT a prime number.");
+            scanner.close();
+        }
     }
 }
-
-
