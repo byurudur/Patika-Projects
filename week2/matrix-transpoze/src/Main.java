3535c287f930273a0bce923646893c818ce9b28b
byurudur <batuhanyurudur@gmail.com>
2024-02-28 17:58:40 +0300

    The codes have been created.

---
+++ week2/matrix-transpoze/src/Main.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a matrix in dimension of 3x3: ");
+        int[][] userMatrix = new int[3][3];
+        int[][] codeMatrix = new int[3][3];
+
+        userMatrix[0][0] = input.nextInt();
+        userMatrix[0][1] = input.nextInt();
+        userMatrix[0][2] = input.nextInt();
+        userMatrix[1][0] = input.nextInt();
+        userMatrix[1][1] = input.nextInt();
+        userMatrix[1][2] = input.nextInt();
+        userMatrix[2][0] = input.nextInt();
+        userMatrix[2][1] = input.nextInt();
+        userMatrix[2][2] = input.nextInt();
+        System.out.println("Your matrix: ");
+
+        for (int i = 0; i < userMatrix.length; i++){
+            for (int j = 0; j < userMatrix[i].length; j++){
+                System.out.print(userMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        codeMatrix[0][0] = userMatrix[0][0];
+        codeMatrix[0][1] = userMatrix[1][0];
+        codeMatrix[0][2] = userMatrix[2][0];
+        codeMatrix[1][0] = userMatrix[0][1];
+        codeMatrix[1][1] = userMatrix[1][1];
+        codeMatrix[1][2] = userMatrix[2][1];
+        codeMatrix[2][0] = userMatrix[0][2];
+        codeMatrix[2][1] = userMatrix[1][2];
+        codeMatrix[2][2] = userMatrix[2][2];
+
+        System.out.println("Your matrix transposed: ");
+
+        for (int i = 0; i < codeMatrix.length; i++){
+            for (int j = 0; j < codeMatrix[i].length; j++){
+                System.out.print(codeMatrix[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
+}
