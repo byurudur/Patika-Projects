65abedda2f87f712a652b88c433d29e1fcc05d32
byurudur <batuhanyurudur@gmail.com>
2024-02-28 18:18:38 +0300

    File name changed, clean code applied

--- week2/matrix-transpoze/src/Main.java
+++ week2/matrix-transpoze/src/Main.java
@@ -1,4 +1,9 @@
-import java.util.Arrays;
+/**
+ * @author Ahmet Batuhan YÜRÜDÜR
+ * @since 2024
+ */
+
+
 import java.util.Scanner;
 
 public class Main {
@@ -17,32 +22,43 @@ public class Main {
         userMatrix[2][0] = input.nextInt();
         userMatrix[2][1] = input.nextInt();
         userMatrix[2][2] = input.nextInt();
-        System.out.println("Your matrix: ");
+        System.out.println("Your matrix transposed: ");
 
         for (int i = 0; i < userMatrix.length; i++){
             for (int j = 0; j < userMatrix[i].length; j++){
-                System.out.print(userMatrix[i][j] + " ");
+                System.out.print(userMatrix[j][i] + " ");
             }
             System.out.println();
+
         }
 
-        codeMatrix[0][0] = userMatrix[0][0];
-        codeMatrix[0][1] = userMatrix[1][0];
-        codeMatrix[0][2] = userMatrix[2][0];
-        codeMatrix[1][0] = userMatrix[0][1];
-        codeMatrix[1][1] = userMatrix[1][1];
-        codeMatrix[1][2] = userMatrix[2][1];
-        codeMatrix[2][0] = userMatrix[0][2];
-        codeMatrix[2][1] = userMatrix[1][2];
-        codeMatrix[2][2] = userMatrix[2][2];
+    /*
 
-        System.out.println("Your matrix transposed: ");
+     Due to clean-coding this part has been commented, can be seen in the initial version of the codes from Github.
 
-        for (int i = 0; i < codeMatrix.length; i++){
-            for (int j = 0; j < codeMatrix[i].length; j++){
-                System.out.print(codeMatrix[i][j]+ " ");
-            }
-            System.out.println();
-        }
+     codeMatrix[0][0] = userMatrix[0][0];
+     codeMatrix[0][1] = userMatrix[1][0];
+     codeMatrix[0][2] = userMatrix[2][0];
+     codeMatrix[1][0] = userMatrix[0][1];
+     codeMatrix[1][1] = userMatrix[1][1];
+     codeMatrix[1][2] = userMatrix[2][1];
+     codeMatrix[2][0] = userMatrix[0][2];
+     codeMatrix[2][1] = userMatrix[1][2];
+     codeMatrix[2][2] = userMatrix[2][2];
+
+     System.out.println("Your matrix transposed: ");
+
+     for (int i = 0; i < codeMatrix.length; i++)
+     {
+     for (int j = 0; j < codeMatrix[i].length; j++)
+     {
+     System.out.print(codeMatrix[i][j]+ " ");
+     }
+     System.out.println();
+     }
+
+    */
+
+
     }
 }
---
+++ week2/matrix-transpoze/src/README.md
@@ -0,0 +1,8 @@
+## 3x3 Dimensioned Matrix Transpose Creator
+
+### Works with given 9 numbers and outputs the transposed matrix to the user.
+#### Comment lines below are the additional (long version) of this code, works if;
+    - System.out.print(userMatrix[j][i] + " "); // row 23
+#### Changed to;
+    -System.out.print(userMatrix[i][j] + " ");
+#### and after that changing the comment lines to codes from row #29 to #54
