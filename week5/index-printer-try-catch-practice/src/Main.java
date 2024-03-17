56ab92f01762b6b33859f37bbdf17f49988b9c6c
byurudur <batuhanyurudur@gmail.com>
2024-03-17 14:50:44 +0300

    Comment lines have been added

---
+++ week5/index-printer-try-catch-practice/src/Main.java
@@ -0,0 +1,62 @@
+/**
+ * @author Ahmet Batuhan YÜRÜDÜR
+ * @since 2024
+ */
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    // Method for eye comfort on the output;
+    public static void bracketEquals() {
+        System.out.println("==================");
+    }
+
+    public static void main(String[] args) {
+
+        // A manually created single-dimensioned list;
+
+        int[] list = new int[10];
+        list[0] = 10;
+        list[1] = 20;
+        list[2] = 30;
+        list[3] = 40;
+        list[4] = 50;
+        list[5] = 60;
+        list[6] = 70;
+        list[7] = 80;
+        list[8] = 90;
+        list[9] = 100;
+
+        // Collecting data from the user;
+        // do-while loop for continuous inputs;
+        // try-catch blocks for exception handling;
+        // Exiting mechanism has assigned on #99;
+
+        System.out.println("To quit, type the number 99 (No item on index #99)");
+        bracketEquals();
+        int itemIndex;
+        do {
+            Scanner input = new Scanner(System.in);
+            System.out.println("I want to learn the item at the index of: ");
+            itemIndex = input.nextInt();
+            if (itemIndex == 99) {
+                bracketEquals();
+                System.out.println("Exiting...");
+                bracketEquals();
+                break;
+            } else {
+                try {
+                    System.out.println("The item at the index " + itemIndex + " is: " + list[itemIndex]);
+                    System.out.println("Another?");
+                    bracketEquals();
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("Index " + itemIndex + " is out of bounds for a list length of 10.");
+                    System.out.println("Try again.");
+                    bracketEquals();
+                }
+            }
+        } while (true);
+    }
+}
